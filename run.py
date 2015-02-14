63cdad1ef0d891007fbed3b2ada208fa0b5c8048
Amit <amit@onefinestay.com>
2015-02-14 23:17:29 +0000

    ability to filter by bus

--- run.py
+++ run.py
@@ -7,15 +7,23 @@ bt = BusTimes()
 
 app = Flask(__name__)
 
-@app.route('/')
-def displayTimes():
+@app.route('/sw')
+def displaySwTimes():
     putney = [50440]
     clj = [75474,77433]
-    return render_template('bustimes.html',
+    return render_template('sw_bustimes.html',
         putney_33=bt.getArrivalTimes(putney),
         putney_disruption=bt.getDisruptionInformation(putney[0]),
         clj_combined=bt.getArrivalTimes(clj),
         clj_disruption=bt.getDisruptionInformation(clj[0]),
+        )
+
+@app.route('/se')
+def displaySeTimes():
+    lewisham = [48893, 47492]
+    bus = ['273', '261']
+    return render_template('se_bustimes.html',
+        lewisham_buses=bt.getArrivalTimes(stopCodes=lewisham,filter=bus)
         )
 
 if __name__ == '__main__':
