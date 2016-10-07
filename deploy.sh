fc93c77ebc917494840b095583989d983afee257
Amit Lalani <amit@onefinestay.com>
2016-10-07 19:17:36 +0100

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SHA1="latest"
+
+REGION="eu-west-1"
+APP="travelhelper"
+DOCKER_IMAGE="145949126156.dkr.ecr.eu-west-1.amazonaws.com/travel-helper"
+TAG=$SHA1
+S3_BUCKET="amit-code"
+S3_DIR=$travelhelper
+
+# Update Dockerrun Template
+sed  "s|<DOCKER_IMAGE>|$DOCKER_IMAGE|g ; s|<TAG>|$SHA1|g" Dockerrun.aws.json.template > Dockerrun.aws.json
+
+# Printing Template
+cat Dockerrun.aws.json
+
+# Copy Template to S3
+aws s3 cp Dockerrun.aws.json s3://$S3_BUCKET/$S3_DIR/Dockerrun.aws.json
+
+# Update EB Application
+aws elasticbeanstalk create-application-version --region=$REGION \
+                                                --application-name $APP \
+                                                --version-label $SHA1 \
+                                                --source-bundle S3Bucket=$S3_BUCKET,S3Key=$S3_DIR/Dockerrun.aws.json
+
